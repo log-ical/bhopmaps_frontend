2863cd5b82a33aa045658827c2fcdf5300b40669
CodeKhenzy <codingkhenzy@gmail.com>
2022-01-24 00:05:57 +0100

    fix

--- src/api/UserContext.ts
+++ src/api/UserContext.ts
@@ -2,4 +2,4 @@ import { createContext } from 'react';
 
 export const UserContext: any = createContext(null);
 
-export const API_URL = 'https://api.bhopmaps.app/api';
+export const API_URL = 'https://api-bhopmaps-husth.ondigitalocean.app/api';
